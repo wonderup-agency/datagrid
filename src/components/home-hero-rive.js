942d34bed2153784be1e6f7597af0a327511e8b7
napu <nahuel.haberkorn@gmail.com>
2025-08-05 17:47:00 -0300

    home hero rive

---
+++ src/components/home-hero-rive.js
@@ -0,0 +1,26 @@
+import * as rive from "@rive-app/canvas";
+
+(() => {
+  const canvas = document.querySelector("#home-hero-rive");
+  if (!canvas) return;
+
+  const layout = new rive.Layout({
+    fit: rive.Fit.Cover,
+    alignment: rive.Alignment.TopCenter,
+  });
+
+  const riveFile =
+    "https://cdn.prod.website-files.com/68831b52fc735726fbf24755/6892657bfe66bd406d1e2757_datagrid__hero.riv";
+    
+  const r = new rive.Rive({
+    src: riveFile,
+    canvas: canvas,
+    layout: layout,
+    autoplay: true,
+    artboard: "Main",
+    stateMachines: "State Machine",
+    onLoad: () => {
+      r.resizeDrawingSurfaceToCanvas();
+    },
+  });
+})();
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import "swiper/css";
 
 import "./components/demo-form.js";
+import "./components/home-hero-rive.js";
 import "./components/agents-slider.js";
 import "./components/stories-slider.js";
