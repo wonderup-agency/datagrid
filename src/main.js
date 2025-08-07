3e9cc8ea7131fc58b818035efbe82c87197051a2
napu <nahuel.haberkorn@gmail.com>
2025-08-07 14:24:23 -0300

    ai tasks

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import "swiper/css";
 
-import "./components/demo-form.js";
-import "./components/home-hero-rive.js";
+import "./components/home-hero.js";
 import "./components/agents-slider.js";
 import "./components/stories-slider.js";
